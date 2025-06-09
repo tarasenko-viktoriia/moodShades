db6524684a1d70c2c7af26862d3429838bdfcaa0
victoriataracenko <victoriataracenko0412@gmail.com>
2025-06-09 16:09:14 +0300

    test fonts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   theme: {
+      extend: {
+         fontFamily: {
+         integral: ['IntegralCF-Bold', 'sans-serif'],
+         satoshi: ['Satoshi-Regular', 'sans-serif'],
+         },
+      },
+   },
+   content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+};
