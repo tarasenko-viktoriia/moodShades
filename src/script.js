8f8196846a7d8eec076e3c4dd0933fa44c065972
victoriataracenko <victoriataracenko0412@gmail.com>
2025-06-10 11:46:32 +0300

    edited hero section Added section with logo

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const img = [
+   'src/img/main-img-01.jpg',
+   'src/img/main-img-02.jpg',
+   'src/img/main-img-03.jpg',
+]
+
+let index = 0
+const imgEl = document.getElementById("slideshow")
+
+setInterval(() => {
+   index = (index +1) % img.length
+   imgEl.src = img[index]
+}, 3000);
+
+
+const copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
